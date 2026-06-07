a72af2fe5699b6af2517a0935208658f62ae930e
Shine Ministry <160314493+shineministry@users.noreply.github.com>
2026-06-07 19:01:11 +0530

    Change greeting from 'Hello' to 'Goodbye'

--- auth.js
+++ auth.js
@@ -16,7 +16,7 @@ let sessionStartTime = null;
 
    let sessionId = crypto.randomUUID();
 window.allFilesData = {};
-window.SHINE_AUTH_VERSION = '20260607-offlinefix3';
+window.SHINE_AUTH_VERSION = '20260607-offlinefix4';
 let currentCategory = "";
    
    async function sha256Bytes(text){
